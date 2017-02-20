bd9f1bf10ad3b2bfdda1000f0bace4fbf8bc2b8c
Alan Gardin <gardin.alan@gmail.com>
2017-02-20 14:40:26 +0100

    Updated graphs searches

--- data structures/graphs/searches.py
+++ data structures/graphs/searches.py
@@ -1,75 +1,72 @@
 # -*- coding: utf-8 -*-
 # !/usr/bin/env python
 
-
-
+import queue
 ## Searches
-# Recursive bfs?
-##
-from graph_implementations import *
+# TODO: Recursive bfs?
+# TODO: Add comments
+# TODO: Add tests
 
-
-
-def dfs_lrec(g, start):
+## BFS
+def bfs_l(g, s):
     """
-    Recursive Depth First Search for adjacency list
+    Breadth First Search for adjacency list.
     
     Returns:
         None
     """
-    def aux(g, vertice):
-        visited.append(vertice)
-        print(vertice)
+    V = []                                                                      # List of visited vertices
+    Q = queue.Queue()
+    Q.put(s)
+    
+    while not Q.empty():
+        v = Q.get()
+        
+        if v not in V:
+            print(v)
+            
+            V.append(v)
+            
+            for u in g[v]:
+                Q.put(u)
 
-        for neighbor in g.getNeighbors(vertice):
-            if neighbor not in visited:
-                aux(g, neighbor)
-                visited.append(neighbor)
+## DFS
+def dfsRec_l(g, s):
+    """
+    Recursive Depth First Search for adjacency lists
+    
+    Returns:
+        None
+    """
+    
+    def visit(v):
+        print(v)
+        V.append(v)
+        for u in g[v]:
+            if u not in V:
+                visit(u)
+    
+    V = []                                                                      # List of visited vertices
+    visit(s)
 
-    visited = []
-    aux(g, start)
 
-
-def dfs_limp(g, start):
+def dfsImp_l(g, s):
     """
      Imperative Depth First Search for adjacency list
     
     Returns:
         None
     """
-    visited = []
-    stack = Stack()
-    stack.push(start)
-
-    while not stack.empty():
-        cur_vertice = stack.pop()
+    V = []                                                                      # List of visited vertices
+    Q = []
+    Q.append(s)                                                       # Stack
+    
+    while not Q == []:
+        v = Q.pop()
         
-        if cur_vertice not in visited:
-            visited.append(cur_vertice)
-            print(cur_vertice)
-    
-            for neighbor in g.getNeighbors(cur_vertice):
-                stack.push(neighbor)
-
-
-def bfs_l(g, start):
-    """
-    Breadth First Search for adjacency list
-    
-    Returns:
-        None
-    """
-    
-    q = Queue()
-    visited = []
-    q.put(start)
-
-    while not q.empty():
-        current_vertice = q.get()
-
-        if current_vertice not in visited:
-            visited.append(current_vertice)
-            print(current_vertice)
-
-            for neighbor in g.getNeighbors(current_vertice):
-                q.put(neighbor)
+        if v not in V:
+            print(v)
+            V.append(v)
+            
+            for u in g[v]:
+                Q.append(u)
