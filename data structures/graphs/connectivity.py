a7bf13713ee736c36fb7174725855667d437a5dd
Alan Gardin <gardin.alan@gmail.com>
2017-02-20 18:36:07 +0100

    Added connectivity test

--- data structures/graphs/connectivity.py
+++ data structures/graphs/connectivity.py
@@ -2,3 +2,28 @@
 # !/usr/bin/env python
 
 ## Connectivity
+# TODO: calcul de composantes connexes
+# TODO: tests de connexité
+
+## Connectivity test
+def connectivityTest(g):
+    """
+        Input: A graph as an adjacency list
+        Uses an imperative dfs
+        Returns:
+            Boolean
+    """
+    V = []                                                                      # List of visited vertices
+    Q = []
+    Q.append(0)                                                                 # Stack
+    
+    while not Q == []:
+        v = Q.pop()
+        
+        if v not in V:
+            V.append(v)
+            
+            for u in g[v]:
+                Q.append(u)
+    
+    return len(V)==len(g)
