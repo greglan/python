f091ad9ea11d2f4045fb7b9c50e1faa770bd5938
Alan Gardin <gardin.alan@gmail.com>
2018-01-29 23:48:45 +0100

    Updated permutations

--- maths/permutations.py
+++ maths/permutations.py
@@ -3,19 +3,19 @@
 """
 
 
-class Permutation(list):
-    def __init__(self, l=[]):
-        self.extend(l)
+class Permutation():
+    def __init__(self, l):
+        self.data = l
 
     def check(self):
-        n = self.__len__()
+        n = len(self.data)
         occurences = [0 for k in range(n)]
         i = 0
         try:
             while i < n:
-                if occurences[self[i]] > 1:
+                occurences[self.data[i]] += 1
+                if occurences[self.data[i]] > 1:
                     return False
-                    occurences[self[i]] += 1
                 i += 1
 
         except IndexError:
@@ -29,20 +29,22 @@ class Permutation(list):
         :type t: Permutation
         :return:
         """
-        n = len(self)
+        n = len(self.data)
         i = [0 for k in range(n)]
 
         for k in range(n):
-            i[self[k]] = k
+            i[self.data[k]] = k
 
         return Permutation(i)
 
-    def __repr__(self):
+    def __str__(self):
         s = '('
-        for k in self:
+        for k in self.data:
             s += str(k) + ' '
 
         return s[:-1] + ')'
+
+    def __repr__(self): return self.__str__()
 
 
 def isPermutation(t):
@@ -64,96 +66,96 @@ def compose(t,u):  # TODO: use a method independent of the length
 
 
 # Plus petit entier k non-nul tel que t^k = id
-def ordre(t):
-    n = len(t)
-    k = 1
-    u = t
-    id = [i for i in range(n)]
-    
-    while u != id:
-        k = k + 1
-        u = compose(u,t)
-    
-    return k
+# def ordre(t):
+#     n = len(t)
+#     k = 1
+#     u = t
+#     id = [i for i in range(n)]
+#
+#     while u != id:
+#         k = k + 1
+#         u = compose(u,t)
+#
+#     return k
+#
+#
+# # Période d’un indice i pour la permutation t: plus petit entier k non nul tel
+# # que t^k (i) = i.
+# def periode(t,i):
+#     k = 1
+#     u = t[i]
+#
+#     while u != i:
+#         k = k + 1
+#         u = t[u]
+#
+#     return k
+#
+#
+# # L’orbite de i pour la permutation t est l’ensemble des indices j tels qu’il
+# # existe k avec: t^k (i) = j.
+# def orbite(t,i):
+#     u = t[i]
+#     orbite = []
+#
+#     while u != i:
+#         orbite.append(u)
+#         u = t[u]
+#
+#     orbite.append(i)
+#     return orbite
+#
+#
+# def estDansOrbite(t,i,j):
+#     ind = t[i]
+#     while ind != i and ind != j:
+#         ind = t[ind]
+#     return ind == j
+#
+#
+#
+# # On suppose que t est au moins un permutation. On calcule le nombre d'indices
+# # ou t diffère de l'identité.
+# def estTransposition(t):
+#     n = len(t)
+#     indices = []
+#
+#     for i in range(n):
+#         if t[i] != i:
+#             indices.append(i)
+#
+#     # Puisqu'il s'agit d'une permutation, les deux indices qui sont différents
+#     # sont nécessairement tels que t[i]=j et t[j]=i
+#     if len(indices)>2:
+#         return False
+#     else:
+#         return True
+#
+#
+# def orbites(t):
+#     n = len(t)
+#     orbites = []
+#     for i in range(n):
+#         orbites.append(orbite(t,i))
+#
+#     # On élimine les doublons
+#
 
-
-# Période d’un indice i pour la permutation t: plus petit entier k non nul tel 
-# que t^k (i) = i.
-def periode(t,i):
-    k = 1
-    u = t[i]
-    
-    while u != i:
-        k = k + 1
-        u = t[u]
-    
-    return k
-
-
-# L’orbite de i pour la permutation t est l’ensemble des indices j tels qu’il 
-# existe k avec: t^k (i) = j.
-def orbite(t,i):
-    u = t[i]
-    orbite = []
-    
-    while u != i:
-        orbite.append(u)
-        u = t[u]
-    
-    orbite.append(i)
-    return orbite
-
-
-def estDansOrbite(t,i,j):
-    ind = t[i]
-    while ind != i and ind != j:
-        ind = t[ind]
-    return ind == j
-
-
-
-# On suppose que t est au moins un permutation. On calcule le nombre d'indices 
-# ou t diffère de l'identité.
-def estTransposition(t):
-    n = len(t)
-    indices = []
-    
-    for i in range(n):
-        if t[i] != i:
-            indices.append(i)
-    
-    # Puisqu'il s'agit d'une permutation, les deux indices qui sont différents 
-    # sont nécessairement tels que t[i]=j et t[j]=i
-    if len(indices)>2:
-        return False
-    else:
-        return True
-
-
-def orbites(t):
-    n = len(t)
-    orbites = []
-    for i in range(n):
-        orbites.append(orbite(t,i))
-    
-    # On élimine les doublons
-
-
-def periodes(t):
-    n = len(t)
-    per = n*[0]
-    for indice in range(n):
-        if êr[indice] == 0: #On a pas encore déterminé la période
-            p = 1
-            j = t[indice]
-            while j != indice: #Détermine la période
-                p+= 1 # p de indice
-                j = t[j]
-            ind = indice
-            for q in range(p): #Recopie la valeur d
-                per[ind] = p #Long de l'orbite de 
-                ind = t[ind]
-    return per
+# def periodes(t):
+#     n = len(t)
+#     per = n*[0]
+#     for indice in range(n):
+#         if êr[indice] == 0: #On a pas encore déterminé la période
+#             p = 1
+#             j = t[indice]
+#             while j != indice: #Détermine la période
+#                 p+= 1 # p de indice
+#                 j = t[j]
+#             ind = indice
+#             for q in range(p): #Recopie la valeur d
+#                 per[ind] = p #Long de l'orbite de
+#                 ind = t[ind]
+#     return per
     
 # def estCycle(t):
 #     t1 = t[:]
@@ -184,13 +186,13 @@ def periodes(t):
 #                 image = t[image]
 #
 # Retourne la liste de toutes les permutations de n entiers (0 exclu)
-def perm(n):
-    if n==1:
-        return [[1]]
-    else:
-        l = perm(n-1)
-        q = []
-        for p in l: #Pour chaque permutation de longueur n-1
-            for k in range(n):
-                q.append(p[:k]+[n]+p[k:])
-        return q
+# def perm(n):
+#     if n==1:
+#         return [[1]]
+#     else:
+#         l = perm(n-1)
+#         q = []
+#         for p in l: #Pour chaque permutation de longueur n-1
+#             for k in range(n):
+#                 q.append(p[:k]+[n]+p[k:])
+#         return q
--- maths/test_permutations.py
+++ maths/test_permutations.py
@@ -1,16 +1,26 @@
 from unittest import TestCase
+
 from maths.permutations import *
 
 
 class TestPermutation(TestCase):
-    def __init__(self):
-        self.permutations = []
+    def setUp(self):
+        self.permutations = list(map(Permutation, [[2, 0],
+                                                   [3, 1],
+                                                   [0, 1, 2, 3],
+                                                   [1, 2, 0, 3]]))
+        self.not_permutations = list(map(Permutation, [[1, 2, 3],
+                                                       ]))
 
     def test_check(self):
-        self.assertTrue(Permutation([0,1,2,3]).check())
-        self.assertTrue(Permutation([1,2,0,3]).check())
-        self.assertFalse(Permutation([1,2,3]).check())
-        #self.fail()
+        for permutation in self.permutations:
+            self.assertTrue(permutation.check())
+
+        for not_permutation in self.not_permutations:
+            self.assertFalse(not_permutation.check())
 
     def test_inverse(self):
-        self.assertTrue()
+        self.assertEqual(self.permutations[0].inverse(), Permutation([]))
+
+    def test___repr_(self):
+        self.assertEqual(self.permutations[0].__repr__(), "(0 1 2 3)")
