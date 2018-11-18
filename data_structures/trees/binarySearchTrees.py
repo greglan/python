bedfc26a4340ba15661a0998029ed2b8b55a441d
Alan Gardin <gardin.alan@gmail.com>
2018-11-18 11:04:16 +0100

    Simplified binary search trees

--- data_structures/trees/binarySearchTrees.py
+++ data_structures/trees/binarySearchTrees.py
@@ -10,29 +10,19 @@ class BinarySearchTree:
         self.left_child = None
         self.right_child = None
 
-    def is_leaf(self):
-        """
-        Indicates whether or not the current tree is a leaf.
-        :return: bool
-        """
-        return (self.left_child is None) and (self.right_child is None) and (self.data is not None)
-
     def search(self, data):
-        if self.is_leaf() and self.data == data:
-            return data
-        else:
-            if data < self.data:
-                if self.left_child is None:
-                    return None
-                else:
-                    return self.left_child.search(data)
-            elif self.data < data:
-                if self.right_child is None:
-                    return None
-                else:
-                    return self.right_child.search(data)
+        if data < self.data:
+            if self.left_child is None:
+                return None
             else:
-                return data
+                return self.left_child.search(data)
+        elif self.data < data:
+            if self.right_child is None:
+                return None
+            else:
+                return self.right_child.search(data)
+        else:
+            return True
 
     def insert(self, data):
         """
@@ -42,40 +32,29 @@ class BinarySearchTree:
         if self.data is None:
             self.data = data
         else:
-            if self.is_leaf():
-                if data < self.data:
+            if data < self.data:
+                if self.left_child is None:
                     self.left_child = BinarySearchTree(data)
                 else:
-                    self.right_child = BinarySearchTree(data)
+                    self.left_child.insert(data)
+
             else:
-                if data < self.data:
-                    if self.left_child is not None:
-                        self.left_child.insert(data)
-                    else:
-                        self.left_child = BinarySearchTree(data)
+                if self.right_child is None:
+                    self.right_child = BinarySearchTree(data)
                 else:
-                    if self.right_child is not None:
-                        self.right_child.insert(data)
-                    else:
-                        self.right_child = BinarySearchTree(data)
+                    self.right_child.insert(data)
 
     def get_min(self):
-        if self.is_leaf():
+        if self.left_child is None:
             return self.data
         else:
-            if self.left_child is None:
-                return self.data
-            else:
-                return self.left_child.get_min()
+            return self.left_child.get_min()
 
     def get_max(self):
-        if self.is_leaf():
+        if self.right_child is None:
             return self.data
         else:
-            if self.right_child is None:
-                return self.data
-            else:
-                return self.right_child.get_max()
+            return self.right_child.get_max()
 
     def delete(self, data):
         # TODO
